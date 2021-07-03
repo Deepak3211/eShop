23dc224685498cadc70fc9d7b49fa7bace21a0a2
deepak3211 <deepakt754@gmail.com>
2021-07-04 01:21:14 +0530

    deployment process done

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 5050;
 if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '../client/build')))  
   app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname,'../client/build/index.html'))
+  res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
   })
 }
 
